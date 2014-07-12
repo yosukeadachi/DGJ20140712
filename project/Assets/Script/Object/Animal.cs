00a92c5def0c606a5755fc16a17e8027720f34a5
yosukeadachi <yosukeadachi@gmail.com>
2014-07-12 19:40:09 +0900

    方向転換

--- project/Assets/Script/Object/Animal.cs
+++ project/Assets/Script/Object/Animal.cs
@@ -38,6 +38,9 @@ public class Animal : MonoBehaviour {
 			Vector2 direction = heading / distance * force;
 			rigidbody2D.velocity = direction;
 
+			//
+			LookAt2D(transform, mTarget, Vector2.up);
+
 			//cola destroy
 			coll.gameObject.SetActive(false);
 		}
@@ -45,5 +48,36 @@ public class Animal : MonoBehaviour {
 			gameObject.SetActive(false);
 		}
 		//
+	}
+
+	/// <summary>
+	/// 指定のオブジェクトの方向に回転する
+	/// </summary>
+	/// <param name="self">Self.</param>
+	/// <param name="target">Target.</param>
+	/// <param name="forward">正面方向</param>
+	public static void LookAt2D(Transform self, Transform target, Vector2 forward)
+	{
+		LookAt2D (self, target.position, forward);
+	}
+	
+	public static void LookAt2D(Transform self, Vector3 target, Vector2 forward)
+	{
+		var forwardDiff = GetForwardDiffPoint (forward);
+		Vector3 direction = target - self.position;
+		float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
+		self.rotation = Quaternion.AngleAxis(angle - forwardDiff, Vector3.forward);
+	}
+	
+	/// <summary>
+	/// 正面の方向の差分を算出する
+	/// </summary>
+	/// <returns>The forward diff point.</returns>
+	/// <param name="forward">Forward.</param>
+	static private float GetForwardDiffPoint(Vector2 forward)
+	{
+		if (Equals (forward, Vector2.up)) return 90;
+		if (Equals (forward, Vector2.right)) return 0;
+		return 0;
 	}
 }
